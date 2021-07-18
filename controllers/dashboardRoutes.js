e60616d33229790c978ef5e55acbe93c08f59d0c
Heather Bjoin <h.m.bjoin@gmail.com>
2021-07-18 11:52:13 -0500

    Adding routes that work

---
+++ controllers/dashboardRoutes.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+const { Post, Comments} = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', withAuth, async (req, res) => {
+    try {
+        const userData = await User.findByPk(req.session.user_id, {
+            attributes: { exclude: ['password'] },
+            include: [{ model: Post }],
+        });
+
+        const user = userData.get({ plain: true });
+
+        res.render(dashboard, {
+            ...user,
+            logged_in: true
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const routes = require("./controllers");
 const helpers = require("./utils/helpers");
 
 const sequelize = require("./config/connection");
-const SequelizeStore = require("connection-session-sequelize")(session.Store);
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({ helpers });
 
 const sess = {
-  secret: "",
+  secret: "secret",
   cookie: {},
   resave: false,
   saveUninitialized: true,
@@ -32,7 +32,7 @@ app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(_dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
